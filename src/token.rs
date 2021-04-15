3a92e9717d7e8b99c4e39803259c9c12b48a3a6b
MadScientist2854 <madscientist2854@gmail.com>
2021-04-15 11:30:14 +0500

    Formatted tokens

--- src/token.rs
+++ src/token.rs
@@ -20,31 +20,93 @@ impl Token {
 
 #[derive(Debug, Clone)]
 pub enum TokenType {
+    // Single-char
     Equal,
-    LeftBracket,
-    RightBracket,
-    LeftParen,
-    RightParen,
+    LeftBracket, RightBracket,
+    LeftParen, RightParen,
     Colon,
     Comma,
-    Dot,
-    Star,
-    Slash,
-    Plus,
-    Minus,
-    Less,
-    Greater,
-    MinusEqual,
+    Period,
+    Star, Slash, Plus, Minus,
+    Less, Greater,
+
+    // Multi-char
+    StarEqual, SlashEqual, PlusEqual, MinusEqual,
+    LessEqual, GreaterEqual, NotEqual,
     Arrow,
+
+    // Keywords
+    //// Primitives
+    BOOLEAN,
+    INTEGER, REAL,
+    CHAR, STRING,
+    DATE,
+    ARRAY,
+    //// Single-line statements
+    DECLARE,
+    CONSTANT,
+    CALL,
+    INPUT, OUTPUT,
+    RETURN,
+    OPENFILE, CLOSEFILE,
+    READFILE, WRITEFILE,
+    GETRECORD,
+    PUTRECORD,
+    SEEK,
+    STEP,
+    //// Blocks
+    PROCEDURE,
+    ENDPROCEDURE,
+    BYREF, BYVALUE,
+    FUNCTION,
+    RETURNS,
+    ENDFUNCTION,
+    FOR,
+    ENDFOR,
+    IF,
+    THEN,
+    ELSE,
+    ENDIF,
+    CASE,
+    ENDCASE,
+    OTHERWISE,
+    REPEAT,
+    UNTIL,
+    WHILE,
+    ENDWHILE,
+    TYPE,
+    ENDTYPE,
+    //// Expression keywords
+    MOD,
+    DIV,
+    AND,
+    OR,
+    NOT,
+    TRUE,
+    FALSE,
+    READ,
+    WRITE,
+    APPEND,
+    RANDOM,
+    EOF,
+    //// Misc
+    OF,
+    TO,
+
+    // Literals
     Literal(Literal),
     Identifier
+
+    // Pre-defined functions
+    // RND
+    // RANDOMBETWEEN
 }
 
 #[derive(Debug, Clone)]
 pub enum Literal {
     Int(i32),
     Float(f32),
-    Char(char),
-    String(String),
-    Date(i8, i8, i16)
+    Char(char), //''
+    String(String), //""
+    Date(i8, i8, i16) // dd/mm/yyyy
 }
